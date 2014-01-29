dd2b56596318d5b03d4677874254ffeb3f15d644
Jimimimi <jimmy.messinis@gmail.com>
2014-01-29 04:25:13 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ App.controller('getData', function($scope, $http) {
   $http.get('./data/data.json')
        .then(function(res){
           App.data = res.data;
+          App.market = new Economia(App.data)
         });
 });
-
-var market = new Economia(App.data);
