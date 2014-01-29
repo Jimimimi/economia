a89e4f528ec4bb10676c116ac84de9d7f41f5efc
Jimimimi <jimmy.messinis@gmail.com>
2014-01-29 04:17:37 +0100

    angular test

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var App = angular.module('economia', ['ui.bootstrap']);
+
+App.controller('getData', function($scope, $http) {
+  $http.get('data/data.json')
+       .then(function(res){
+          App.data = res.data;
+        });
+});
+
+var market = new Economia(App.data);
