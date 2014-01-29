9fbc3e3b6cbe71d443df322d98228ff5f32958cc
Jimimimi <jimmy.messinis@gmail.com>
2014-01-29 04:19:28 +0100

    asdf

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var App = angular.module('economia', ['ui.bootstrap']);
 
 App.controller('getData', function($scope, $http) {
-  $http.get('data/data.json')
+  $http.get('./data/data.json')
        .then(function(res){
           App.data = res.data;
         });
