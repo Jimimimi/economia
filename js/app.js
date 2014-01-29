103f6814780e946055c94699e1a1f992a2b7f61f
Jimimimi <jimmy.messinis@gmail.com>
2014-01-29 10:26:55 +0100

    Ractive setup

--- js/app.js
+++ js/app.js
@@ -1,9 +1,53 @@
-var App = angular.module('economia', ['ui.bootstrap']);
 
-App.controller('getData', function($scope, $http) {
-  $http.get('./data/data.json')
-       .then(function(res){
-          App.data = res.data;
-          App.market = new Economia(App.data)
-        });
-});
+var market, 
+	JSONdata,
+	round = 0,
+	views = {
+		market:new Ractive({
+				el: 'market',
+				template: '#marketTemplate'
+		}),
+		render: function(market){
+			round += 1;
+			views.market.set({
+				round: round
+			});
+			views.agents.set({
+				agents: market.agents
+			});
+			views.commodities.set({
+				commodities: getCommodityData()
+			})
+		},
+		agents: new Ractive({
+			el: 'agents',
+			template: '#agentsTemplate'
+		}),
+		commodities: new Ractive({
+			el: 'commodities',
+			template: '#commoditiesTemplate'
+		})
+	};
+
+function getCommodityData(){
+	var arr = [];
+	market.commodities.forEach(function(commodity){
+		var avg = market.getAvgPrice(commodity.name);
+		var diff = avg - market.history.price[commodity.name][market.history.price[commodity.name].length-2]
+		var c = {
+			name: commodity.name,
+			priceAvg: avg,
+			diff: diff
+		};
+		arr.push(c);
+	})
+	return arr;
+}
+
+$.getJSON('data/data.json', function(data){
+	JSONdata = data;
+	market = new Economia(data);
+
+	market.tick(views.render(market)); // first render;
+})
+
